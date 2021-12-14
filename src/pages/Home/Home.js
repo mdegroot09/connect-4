2a3efb06584c20a2bc2ae3cce4082f16090e854d
mdegroot09 <mdegroot09@gmail.com>
2021-12-13 17:04:23 -0700

    Added board layout

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,10 +5,75 @@ class Home extends Component {
         super(props);
         this.state = {  }
     }
+
     render() { 
         return ( 
             <div className='home'>
-                home
+                <div className='board'>
+                    <div className='row'>
+                        <div className='slot' id='slot0'></div>
+                        <div className='slot' id='slot1'></div>
+                        <div className='slot' id='slot2'></div>
+                        <div className='slot' id='slot3'></div>
+                        <div className='slot' id='slot4'></div>
+                        <div className='slot' id='slot5'></div>
+                        <div className='slot' id='slot6'></div>
+                    </div>
+                    <div className='row'>
+                        <div className='slot' id='slot7'></div>
+                        <div className='slot' id='slot8'></div>
+                        <div className='slot' id='slot9'></div>
+                        <div className='slot' id='slot10'></div>
+                        <div className='slot' id='slot11'></div>
+                        <div className='slot' id='slot12'></div>
+                        <div className='slot' id='slot13'></div>
+                    </div>
+                    <div className='row'>
+                        <div className='slot' id='slot14'></div>
+                        <div className='slot' id='slot15'></div>
+                        <div className='slot' id='slot16'></div>
+                        <div className='slot' id='slot17'></div>
+                        <div className='slot' id='slot18'></div>
+                        <div className='slot' id='slot19'></div>
+                        <div className='slot' id='slot20'></div>
+                    </div>
+                    <div className='row'>
+                        <div className='slot' id='slot21'></div>
+                        <div className='slot' id='slot22'></div>
+                        <div className='slot' id='slot23'></div>
+                        <div className='slot' id='slot24'></div>
+                        <div className='slot' id='slot25'></div>
+                        <div className='slot' id='slot26'></div>
+                        <div className='slot' id='slot27'></div>
+                    </div>
+                    <div className='row'>
+                        <div className='slot' id='slot28'></div>
+                        <div className='slot' id='slot29'></div>
+                        <div className='slot' id='slot30'></div>
+                        <div className='slot' id='slot31'></div>
+                        <div className='slot' id='slot32'></div>
+                        <div className='slot' id='slot33'></div>
+                        <div className='slot' id='slot34'></div>
+                    </div>
+                    <div className='row'>
+                        <div className='slot' id='slot35'></div>
+                        <div className='slot' id='slot36'></div>
+                        <div className='slot' id='slot37'></div>
+                        <div className='slot' id='slot38'></div>
+                        <div className='slot' id='slot39'></div>
+                        <div className='slot' id='slot40'></div>
+                        <div className='slot' id='slot41'></div>
+                    </div>
+                    <div className='row'>
+                        <div className='slot' id='slot42'></div>
+                        <div className='slot' id='slot43'></div>
+                        <div className='slot' id='slot44'></div>
+                        <div className='slot' id='slot45'></div>
+                        <div className='slot' id='slot46'></div>
+                        <div className='slot' id='slot47'></div>
+                        <div className='slot' id='slot48'></div>
+                    </div>
+                </div>
             </div>
          );
     }
